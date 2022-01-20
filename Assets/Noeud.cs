b72fe0b2d07bc3a6d6a2824bd757085df6635bca
Félix Tellitocci <73948501+Magiccarpet00@users.noreply.github.com>
2022-01-20 20:39:33 +0100

    idée de noeud

---
+++ Assets/Noeud.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noeud : MonoBehaviour
+{
+    public bool depart;
+    public bool arriveePotentiel;
+    public bool arrivee;
+    public bool surCheminCritique;
+    public bool estNoeudDisjoint;
+    public bool utiliser;  
+
+}
