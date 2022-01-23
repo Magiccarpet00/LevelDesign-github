1b0a78c2b7191982bafaf3c4b5a6b6c69d9168f2
Félix Tellitocci <73948501+Magiccarpet00@users.noreply.github.com>
2022-01-23 23:19:30 +0100

    Implementation de la structure de donné
    je crois que c'est pas trop mal

--- Assets/Noeud.cs
+++ Assets/Noeud.cs
@@ -4,11 +4,133 @@ using UnityEngine;
 
 public class Noeud : MonoBehaviour
 {
+
+    public int x;
+    public int y;
+
+    public List<GameObject> voisins = new List<GameObject>();
+    public static float X_OFFSET = 1f;
+    public static float Y_OFFSET = 1.6f;
+
+    // On va ptet utilse ce type de bool mais pas sur
     public bool depart;
     public bool arriveePotentiel;
     public bool arrivee;
     public bool surCheminCritique;
     public bool estNoeudDisjoint;
-    public bool utiliser;  
+    public bool utiliser;
 
+    public void Start()
+    {
+        Renomage();  // renome le nom du noeud avec les coordonnes X;Y
+        RegarderVoisins();
+    }
+
+    public void Renomage()
+    {
+        this.name = "noeud " + x + ";" + y;
+    }
+
+    public void RegarderVoisins()
+    {
+        Vector2 posCheckVoisin1 = new Vector2(transform.position.x + X_OFFSET*2, transform.position.y);             //droite droite
+        Vector2 posCheckVoisin2 = new Vector2(transform.position.x + X_OFFSET, transform.position.y - Y_OFFSET);    //droite bas
+        Vector2 posCheckVoisin3 = new Vector2(transform.position.x - X_OFFSET, transform.position.y - Y_OFFSET);    //gauche bas
+        Vector2 posCheckVoisin4 = new Vector2(transform.position.x - X_OFFSET*2, transform.position.y);             //gauche gauche
+        Vector2 posCheckVoisin5 = new Vector2(transform.position.x - X_OFFSET, transform.position.y + Y_OFFSET);    //gauche haut
+        Vector2 posCheckVoisin6 = new Vector2(transform.position.x + X_OFFSET, transform.position.y + Y_OFFSET);    //droite haut
+
+        float radiusCircle = 0.2f;
+        Collider2D collider_voisin1 = Physics2D.OverlapCircle(posCheckVoisin1, radiusCircle);
+        Collider2D collider_voisin2 = Physics2D.OverlapCircle(posCheckVoisin2, radiusCircle);
+        Collider2D collider_voisin3 = Physics2D.OverlapCircle(posCheckVoisin3, radiusCircle);
+        Collider2D collider_voisin4 = Physics2D.OverlapCircle(posCheckVoisin4, radiusCircle);
+        Collider2D collider_voisin5 = Physics2D.OverlapCircle(posCheckVoisin5, radiusCircle);
+        Collider2D collider_voisin6 = Physics2D.OverlapCircle(posCheckVoisin6, radiusCircle);
+
+        if (collider_voisin1 != null)
+        {
+            if (collider_voisin1.CompareTag("noeud"))
+            {
+                this.voisins.Add(collider_voisin1.gameObject);
+            }
+        }
+
+        if (collider_voisin2 != null)
+        {
+            if (collider_voisin2.CompareTag("noeud"))
+            {
+                this.voisins.Add(collider_voisin2.gameObject);
+            }
+        }
+
+        if (collider_voisin3 != null)
+        {
+            if (collider_voisin3.CompareTag("noeud"))
+            {
+                this.voisins.Add(collider_voisin3.gameObject);
+            }
+        }
+
+        if (collider_voisin4 != null)
+        {
+            if (collider_voisin4.CompareTag("noeud"))
+            {
+                this.voisins.Add(collider_voisin4.gameObject);
+            }
+        }
+
+        if (collider_voisin5 != null)
+        {
+            if (collider_voisin5.CompareTag("noeud"))
+            {
+                this.voisins.Add(collider_voisin5.gameObject);
+            }
+        }
+
+        if (collider_voisin6 != null)
+        {
+            if (collider_voisin6.CompareTag("noeud"))
+            {
+                this.voisins.Add(collider_voisin6.gameObject);
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+    public void regarderTuilleVoisine_OLD()
+    {
+        Vector2 checkVoisin1 = new Vector2(transform.position.x + 2f, transform.position.y);
+        Vector2 checkVoisin2 = new Vector2(transform.position.x - 2f, transform.position.y);
+
+        Collider2D cd = Physics2D.OverlapCircle(checkVoisin1, 0.3f);
+        Collider2D cd2 = Physics2D.OverlapCircle(checkVoisin2, 0.3f);
+
+        
+
+        if (cd.CompareTag("noeud"))
+        {
+            Debug.Log("PD VA !" + cd);
+            Noeud n = cd.GetComponent<Noeud>();
+            n.depart = true;            
+        }
+
+
+        if(cd2 != null)
+        {
+            if (cd2.CompareTag("noeud"))
+            {
+                Debug.Log("Je t'aime" + cd);
+                Noeud n = cd.GetComponent<Noeud>();
+                n.arrivee = true;
+            }
+        }      
+
+    }
 }
---
+++ Assets/TestMainCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMainCollision : MonoBehaviour
+{
+
+    public GameObject[] noeud; 
+
+    void Start()
+    {
+        /*
+        for (int i = 0; i < noeud.Length; i++)
+        {
+            Noeud n = noeud[i].GetComponent<Noeud>();
+
+            n.regarderCote();
+        }
+        
+
+        Noeud n = noeud[0].GetComponent<Noeud>();
+        n.regarderTuilleVoisine();
+        */
+    }
+
+    
+}
