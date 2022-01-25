d8a5e3eb3e5f7e9cad19bbdc7fce85d55268cec8
Benoît Cante <cantebenoit@hotmail.com>
2022-01-25 19:59:34 +0100

    je suis pas un salaud

--- Assets/Pathfinding.cs
+++ Assets/Pathfinding.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,10 +11,15 @@ public class Pathfinding
     {
         this.noeud = noeud;
     }
-    public void trouveChemin(int startX, int startY, int endX, int endY)
+    public List<GameObject> trouveChemin(int startX, int startY, int endX, int endY)
     {
+     
         GameObject startNode, endNode = null;
         startNode = new GameObject();
+
+        List<GameObject> openList = new List<GameObject> { startNode }; // Notation pour rajouter un noeud, stylé
+        List<GameObject> closedList = new List<GameObject>();
+
         foreach (var n in noeud)
         {
             Noeud v = n.GetComponent<Noeud>();
@@ -41,9 +47,63 @@ public class Pathfinding
         startNode.GetComponent<Noeud>().CalculateFCost();
         bool endNotFound = true;
 
-        
+        // boucle principale on va déplier toute notre liste de noeuds à étudier, la remplir, et sortir les noeuds déjà étudiés
+        while (openList.Count > 0 && endNotFound) // on devra trouver une autre condition d'arrêt
+        {
+            GameObject currentNode = GetLowestFCostNode(openList);
+            if (currentNode == endNode)
+            {
+                // on est arrivé !!
+                endNotFound = false;
+                return CalculChemin(endNode);
+            }
+            openList.Remove(currentNode); // on enlève le noeud à l'étude des noeuds à étudier ... 
+            closedList.Add(currentNode); // .. et on le rajoute à la liste des noeuds déjà étudiés
 
+            List<GameObject> nodes = currentNode.GetComponent<Noeud>().voisins; // --'
+            foreach (GameObject neighborNode in nodes)
+            {
+                float tentativeGCost = currentNode.GetComponent<Noeud>().coutG + CalculDistance(currentNode, neighborNode);
+                if (tentativeGCost < neighborNode.GetComponent<Noeud>().coutG)
+                {
+                    neighborNode.GetComponent<Noeud>().cameFromNode = currentNode.GetComponent<Noeud>();
+                    neighborNode.GetComponent<Noeud>().coutG = tentativeGCost;
+                    neighborNode.GetComponent<Noeud>().coutH = CalculDistance(neighborNode, endNode);
+                    neighborNode.GetComponent<Noeud>().CalculateFCost();
+
+                    if (!openList.Contains(neighborNode))
+                    {
+                        openList.Add(neighborNode);
+                    }
+                }
+
+            }
+        return new List<GameObject>(); // si on a R
     }
+
+
+
+    /** 
+     * A faire par Félix ;) 
+     * */
+    private List<GameObject> CalculChemin(GameObject endNode)
+    {
+        return null;
+    }
+
+    private GameObject GetLowestFCostNode(List<GameObject> openList)
+    {
+        GameObject pn = openList[0];
+        for (int i = 1; i < openList.Count; i++)
+        {
+            if (openList[i].GetComponent<Noeud>().coutF < pn.GetComponent<Noeud>().coutF)
+            {
+                pn = openList[i];
+            }
+        }
+        return pn;
+    }
+
     private float CalculDistance(GameObject a, GameObject b) // hCost
     {
         Vector2 distance= a.transform.position - b.transform.position;
